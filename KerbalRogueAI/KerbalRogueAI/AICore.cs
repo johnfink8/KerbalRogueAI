6a7658f0a70175d774588f41741b9f0d861082ce
John <jfink@lenovo.com>
2015-12-20 21:01:13 -0500

    SSTA works, nothing else is done

---
+++ KerbalRogueAI/KerbalRogueAI/AICore.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MuMech;
+using System.IO;
+using System.Linq.Expressions;
+using System.Reflection;
+using UnityEngine;
+using KSP.IO;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+using File = KSP.IO.File;
+
+namespace KerbalRogueAI
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class AICore : MonoBehaviour
+    {
+        private static Vessel vessel { get { return FlightGlobals.ActiveVessel; } }
+
+        private static MuMech.MechJebCore core { get { return VesselExtensions.GetModules<MuMech.MechJebCore>(vessel)[0]; } }
+
+        private static double atmHeight { get { return vessel.mainBody.atmosphereDepth; } }
+
+        void CheckVessel()
+        {
+            
+            if (vessel != null)
+            {
+                vessel.OnFlyByWire -= flyByWire; //just a safety precaution to avoid duplicates
+                vessel.OnFlyByWire += flyByWire;
+            }
+
+        }
+        SSTOSituation prevSituation = SSTOSituation.Preflight;
+        string strSituation="";
+        string strAp="";
+
+
+        public enum SSTOSituation { Preflight, Takeoff, SubsonicAscent, SonicTransition, HypersonicAscent, CoastToSpace, Circularize,InOrbit }
+        void Start()  //Called when vessel is placed on the launchpad
+        {
+            if ((windowPos.x == 0) && (windowPos.y == 0))//windowPos is used to position the GUI window, lets set it in the center of the screen
+            {
+                windowPos = new Rect(Screen.width / 2, Screen.height / 2, 10, 10);
+            } 
+            RenderingManager.AddToPostDrawQueue(3, new Callback(drawGUI));//start the GUI
+            //at the beginning of the flight, register your fly-by-wire control function that will be called repeatedly during flight:
+            //vessel.OnFlyByWire += new FlightInputCallback(flyByWire);
+
+            //...
+        }
+
+        private SSTOSituation getVesselSituation()
+        {
+            if (Staging.CurrentStage == Staging.StageCount)
+            {
+                //Debug.Log("RogueAI SSTOSituation PreFlight");
+                return SSTOSituation.Preflight;
+            }
+            if (vessel.Landed && vessel.heightFromTerrain < 5.0f)
+            {
+                //Debug.Log("RogueAI SSTOSituation Takeoff");
+                return SSTOSituation.Takeoff;
+            }
+            if (vessel.altitude < 10000 && vessel.mach < 1)
+            {
+                //Debug.Log("RogueAI SSTOSituation SubsonicAscent height " + vessel.altitude);
+                return SSTOSituation.SubsonicAscent;
+            }
+            if (vessel.mach >= 0.9f && vessel.mach < 2)
+            {
+                //Debug.Log("RogueAI SSTOSituation SonicTransition");
+                return SSTOSituation.SonicTransition;
+            }
+            if (vessel.mach >= 1.9 && vessel.orbit.ApA <= atmHeight + 5500)
+            {
+                if (vessel.orbit.ApA > atmHeight+5000 && prevSituation == SSTOSituation.CoastToSpace)
+                    return SSTOSituation.CoastToSpace;
+                //Debug.Log("RogueAI SSTOSituation HypersonicAscent");
+                return SSTOSituation.HypersonicAscent;
+            }
+            if (vessel.orbit.ApA > atmHeight + 5000 && vessel.altitude < atmHeight)
+            {
+                return SSTOSituation.CoastToSpace;
+            }
+            if (vessel.orbit.ApA > atmHeight && vessel.orbit.PeA < atmHeight)
+                return SSTOSituation.Circularize;
+            return SSTOSituation.InOrbit;
+
+        }
+
+        private void holdFlightHeading(FlightCtrlState s, float elevation, float heading)
+        {
+
+            core.attitude.attitudeTo(Quaternion.Euler(elevation,heading,0), AttitudeReference.SURFACE_NORTH, this);
+        }
+
+        private void holdPrograde()
+        {
+            core.attitude.attitudeTo(Quaternion.Euler(0, 0, 0), AttitudeReference.ORBIT, this);
+        }
+
+        private void flyByWire(FlightCtrlState s)
+        {
+            SSTOSituation situation = getVesselSituation();
+            strSituation = situation.ToString();
+            strAp = vessel.orbit.ApA.ToString();
+            switch (situation)
+            {
+                case SSTOSituation.Preflight:
+                    s.mainThrottle = 1;
+                    Staging.ActivateNextStage();
+                    break;
+                case SSTOSituation.Takeoff:
+                    s.mainThrottle = 1;
+                    holdFlightHeading(s,0,90); 
+                    break;
+                case SSTOSituation.SubsonicAscent:
+                    s.mainThrottle = 1;
+                    holdFlightHeading(s, -20, 90);
+                    //DeployLandingGears(false);
+                    if (vessel.ActionGroups[KSPActionGroup.Gear])
+                        vessel.ActionGroups.SetGroup(KSPActionGroup.Gear, false);
+                    break;
+                case SSTOSituation.SonicTransition:
+                    s.mainThrottle = 1;
+                    holdFlightHeading(s, -5, 90);
+                    break;
+                case SSTOSituation.HypersonicAscent:
+                    s.mainThrottle = 1;
+                    holdFlightHeading(s, -20, 90);
+                    break;
+                case SSTOSituation.CoastToSpace:
+                    s.mainThrottle = 0;
+                    holdFlightHeading(s, 0, 90);
+                    break;
+                case SSTOSituation.Circularize:
+                    if (!vessel.patchedConicSolver.maneuverNodes.Any())
+                    {
+                        s.mainThrottle = 0;
+                        Operation circularize = new OperationCircularize();
+                        var computedNode = circularize.MakeNode(vessel.orbit, Planetarium.GetUniversalTime(), core.target);
+                        vessel.PlaceManeuverNode(vessel.orbit, computedNode.dV, computedNode.UT);
+                        core.node.ExecuteOneNode(this);
+                    }
+                    break;
+
+            }
+            prevSituation = situation;
+
+        }
+        /*
+         * Called after the scene is loaded.
+         */
+        void Awake()
+        {
+            Debug.Log("TAC Examples-SimplePartlessPlugin [" + this.GetInstanceID().ToString("X")
+                + "][" + Time.time.ToString("0.0000") + "]: Awake: " + this.name);
+        }
+        /*
+ * Called every frame
+ */
+        void Update()
+        {
+            CheckVessel();
+            if ((Time.time - lastUpdate) > logInterval)
+            {
+                lastUpdate = Time.time;
+                Debug.Log("TAC Examples-SimplePartlessPlugin [" + this.GetInstanceID().ToString("X")
+                    + "][" + Time.time.ToString("0.0000") + "]: Update");
+            }
+        }
+
+        /*
+         * Called at a fixed time interval determined by the physics time step.
+         */
+        private float lastUpdate = 0.0f;
+        private float lastFixedUpdate = 0.0f;
+        private float logInterval = 5.0f;
+        void FixedUpdate()
+        {
+            if ((Time.time - lastFixedUpdate) > logInterval)
+            {
+                lastFixedUpdate = Time.time;
+                Debug.Log("TAC Examples-SimplePartlessPlugin [" + this.GetInstanceID().ToString("X")
+                    + "][" + Time.time.ToString("0.0000") + "]: FixedUpdate");
+            }
+        }
+
+        /*
+         * Called when the game is leaving the scene (or exiting). Perform any clean up work here.
+         */
+        void OnDestroy()
+        {
+            Debug.Log("TAC Examples-SimplePartlessPlugin [" + this.GetInstanceID().ToString("X")
+                + "][" + Time.time.ToString("0.0000") + "]: OnDestroy");
+        }
+
+        protected Rect windowPos;
+
+        private void WindowGUI(int windowID)
+        {
+            GUIStyle mySty = new GUIStyle(GUI.skin.button);
+            mySty.normal.textColor = mySty.focused.textColor = Color.white;
+            mySty.hover.textColor = mySty.active.textColor = Color.yellow;
+            mySty.onNormal.textColor = mySty.onFocused.textColor = mySty.onHover.textColor = mySty.onActive.textColor = Color.green;
+            mySty.padding = new RectOffset(8, 8, 8, 8);
+
+            GUILayout.BeginVertical();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Situation", GUILayout.ExpandWidth(true));
+            GUILayout.Label(strSituation, GUILayout.ExpandWidth(true));
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Apoapsis", GUILayout.ExpandWidth(true));
+            GUILayout.Label(strAp, GUILayout.ExpandWidth(true));
+            GUILayout.EndHorizontal();
+
+            GUILayout.EndVertical();
+
+
+            //DragWindow makes the window draggable. The Rect specifies which part of the window it can by dragged by, and is 
+            //clipped to the actual boundary of the window. You can also pass no argument at all and then the window can by
+            //dragged by any part of it. Make sure the DragWindow command is AFTER all your other GUI input stuff, or else
+            //it may "cover up" your controls and make them stop responding to the mouse.
+            GUI.DragWindow(new Rect(0, 0, 10000, 20));
+
+        }
+        private void drawGUI()
+        {
+            GUI.skin = HighLogic.Skin;
+            windowPos = GUILayout.Window(1, windowPos, WindowGUI, "Set Direction", GUILayout.MinWidth(200));
+        }
+    }
+}
---
+++ KerbalRogueAI/KerbalRogueAI/DisplayModule.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MuMech
+{
+    public class DisplayModule : ComputerModule
+    {
+        public bool hidden = false;
+        public bool locked = false;
+
+        public Rect windowPos
+        {
+            get
+            {
+                if (!HighLogic.LoadedSceneIsEditor)                
+                    return new Rect(windowVector.x, windowVector.y, windowVector.z, windowVector.w);                
+                else
+                    return new Rect(windowVectorEditor.x, windowVectorEditor.y, windowVectorEditor.z, windowVectorEditor.w);
+            }
+            set
+            {
+                if (!HighLogic.LoadedSceneIsEditor)
+                {
+                    windowVector = new Vector4(
+                        Math.Min(Math.Max(value.x, 0), GuiUtils.scaledScreenWidth - value.width),
+                        Math.Min(Math.Max(value.y, 0), GuiUtils.scaledScreenHeight - value.height),
+                        value.width, value.height
+                    );
+                    windowVector.x = Mathf.Clamp(windowVector.x, 10 - value.width, GuiUtils.scaledScreenWidth - 10);
+                    windowVector.y = Mathf.Clamp(windowVector.y, 10 - value.height, GuiUtils.scaledScreenHeight - 10);
+                }
+                else
+                {
+                    windowVectorEditor = new Vector4(
+                        Math.Min(Math.Max(value.x, 0), GuiUtils.scaledScreenWidth - value.width),
+                        Math.Min(Math.Max(value.y, 0), GuiUtils.scaledScreenHeight - value.height),
+                        value.width, value.height
+                    );
+                    windowVectorEditor.x = Mathf.Clamp(windowVectorEditor.x, 10 - value.width, GuiUtils.scaledScreenWidth - 10);
+                    windowVectorEditor.y = Mathf.Clamp(windowVectorEditor.y, 10 - value.height, GuiUtils.scaledScreenHeight - 10);
+                }
+            }
+        }
+
+        [Persistent(pass = (int)Pass.Global)]
+        public Vector4 windowVector = new Vector4(10, 40, 0, 0); //Persistence is via a Vector4 since ConfigNode doesn't know how to serialize Rects
+
+        [Persistent(pass = (int)Pass.Global)]
+        public Vector4 windowVectorEditor = new Vector4(10, 40, 0, 0); //Persistence is via a Vector4 since ConfigNode doesn't know how to serialize Rects
+
+
+        [Persistent(pass = (int)Pass.Global)]
+        public bool showInFlight = true;
+
+        [Persistent(pass = (int)Pass.Global)]
+        public bool showInEditor = false;
+
+        public bool showInCurrentScene { get { return (HighLogic.LoadedSceneIsEditor ? showInEditor : showInFlight); } }
+
+        public int ID;
+        public static int nextID = 72190852;
+
+        public DisplayModule(MechJebCore core)
+            : base(core)
+        {
+            ID = nextID;
+            nextID++;
+        }
+
+        public virtual GUILayoutOption[] WindowOptions()
+        {
+            return new GUILayoutOption[0];
+        }
+
+        protected void WindowGUI(int windowID, bool draggable)
+        {
+            if (GUI.Button(new Rect(windowPos.width - 18, 2, 16, 16), ""))
+            {
+                enabled = false;
+            }
+
+//            if (GUI.Button(new Rect(windowPos.width - 40, 2, 20, 16), (locked ? "X" : "O"))) // lock button needs an icon, letters look crap
+//            {
+//                locked = !locked;
+//            }
+            
+            bool allowDrag = true;
+            if (core.settings.useTitlebarDragging)
+            {
+                float x = Mouse.screenPos.x / GuiUtils.scale;
+                float y = Mouse.screenPos.y / GuiUtils.scale;
+                allowDrag = x >= windowPos.xMin + 3 && x <= windowPos.xMin + windowPos.width - 3 &&
+                            y >= windowPos.yMin + 3 && y <= windowPos.yMin + 17;
+            }
+
+            if (draggable && allowDrag)
+                GUI.DragWindow();
+        }
+
+        protected virtual void WindowGUI(int windowID)
+        {
+            WindowGUI(windowID, true);
+        }
+
+        public virtual void DrawGUI(bool inEditor)
+        {
+            if (showInCurrentScene)
+            {
+                windowPos = GUILayout.Window(ID, windowPos, WindowGUI, GetName(), WindowOptions());
+
+                //                var windows = core.GetComputerModules<DisplayModule>(); // on ice until there's a way to find which window is active, unless you like dragging other windows by snapping
+                //                
+                //                foreach (var w in windows)
+                //                {
+                //                	if (w == this) { continue; }
+                //                	
+                //                	var diffL = (w.windowPos.x + w.windowPos.width - 2) - windowPos.x;
+                //                	var diffR = w.windowPos.x - (windowPos.x + windowPos.width + 2);
+                //                	var diffT = (w.windowPos.y + w.windowPos.height - 2) - windowPos.y;
+                //                	var diffB = w.windowPos.y - (windowPos.y + windowPos.height + 2);
+                //                	
+                //                	if (Math.Abs(diffL) <= 8)
+                //                	{
+                //                		SetPos(w.windowPos.x + w.windowPos.width - 2, windowPos.y);
+                //                	}
+                //                	
+                //                	if (Math.Abs(diffR) <= 8)
+                //                	{
+                //                		SetPos(w.windowPos.x - windowPos.width + 2, windowPos.y);
+                //                	}
+                //                }
+            }
+        }
+
+        public override void OnSave(ConfigNode local, ConfigNode type, ConfigNode global)
+        {
+            base.OnSave(local, type, global);
+
+            if (global != null) global.AddValue("enabled", enabled);
+//            if (global != null) global.AddValue("locked", locked);
+        }
+
+        public override void OnLoad(ConfigNode local, ConfigNode type, ConfigNode global)
+        {
+            base.OnLoad(local, type, global);
+
+            if (global != null && global.HasValue("enabled"))
+            {
+                bool loadedEnabled;
+                if (bool.TryParse(global.GetValue("enabled"), out loadedEnabled)) enabled = loadedEnabled;
+            }
+
+//            if (global != null && global.HasValue("locked"))
+//            {
+//                bool loadedLocked;
+//                if (bool.TryParse(global.GetValue("locked"), out loadedLocked)) locked = loadedLocked;
+//            }
+        }
+
+        public virtual bool isActive()
+        {
+            ComputerModule[] makesActive = { core.attitude, core.thrust, core.rover, core.node, core.rcs, core.rcsbal };
+
+            bool active = false;
+            foreach (var m in makesActive)
+            {
+                if (m != null)
+                {
+                    if (active |= m.users.RecursiveUser(this)) break;
+                }
+            }
+            return active;
+        }
+
+        public override void UnlockCheck()
+        {
+            if (!unlockChecked)
+            {
+                bool prevEn = enabled;
+                enabled = true;
+                base.UnlockCheck();
+                if (unlockParts.Trim().Length > 0 || unlockTechs.Trim().Length > 0 || !IsSpaceCenterUpgradeUnlocked())
+                {
+                    hidden = !enabled;
+                    if (hidden)
+                    {
+                        prevEn = false;
+                    }
+                }
+                enabled = prevEn;
+            }
+        }
+
+        public virtual string GetName()
+        {
+            return "Display Module";
+        }
+    }
+}
